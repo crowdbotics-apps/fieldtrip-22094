2b7310c1d7422083e504ce45f86a3a23e54b2f35
Crowdbotics <team@crowdbotics.com>
2020-10-29 18:13:23 +0000

    Update models

--- backend/fieldtrip_22094/wsgi.py
+++ backend/fieldtrip_22094/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fieldtrip_22094.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fieldtrip_22094.settings")
 
 application = get_wsgi_application()
